0006b81b48b97ffb9ca29cab19be72ee4b23fe25
Lucas Van Tilburg <lucasvantilburg101202@gmail.com>
2021-06-26 19:43:18 +1000

    made home view

---
+++ Learning App/Views/HomeRowView.swift
@@ -0,0 +1,77 @@
+//
+//  HomeRowView.swift
+//  Learning App
+//
+//  Created by Lucas Van Tilburg on 26/6/21.
+//
+
+import SwiftUI
+
+struct HomeRowView: View {
+    
+    var image: String
+    var title: String
+    var description: String
+    var count: String
+    var time: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+                
+            
+            HStack {
+                //Image
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                
+                Spacer()
+                
+                //Text
+                VStack (alignment: .leading, spacing: 10) {
+                    //Headline
+                    Text(title)
+                        .bold()
+                    
+                    //Description
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    
+                    //Icons
+                    HStack {
+                        //Number of lessons/questions
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(.system(size: 10))
+                        
+                        Spacer()
+                        
+                        //Time
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(.system(size: 10))
+                    }
+                }
+                .padding(.leading, 20)
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct HomeRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeRowView(image: "swift", title: "Learn Swift", description: "some description", count: "10 Lessons", time: "2 Hours")
+    }
+}
