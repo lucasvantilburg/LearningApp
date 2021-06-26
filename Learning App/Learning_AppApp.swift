0d099d1e92ba4b02623916fdf2132e9be8122f01
Lucas Van Tilburg <lucasvantilburg101202@gmail.com>
2021-06-26 14:27:54 +1000

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Lucas Van Tilburg on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
